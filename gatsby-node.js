cd56601deba49ff61046b2075a43488b248d3db4
Maurits De Muer <maurits.demuer@student.ap.be>
2021-01-25 22:51:47 +0100

    and i boop

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,44 @@
+const { graphql } = require("gatsby")
+const path = require("path")
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 /* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+  {
+      wpcontent {
+        games {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    }
+    
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
+
+    const games = result.data.wpcontent.games.edges;
+    games.forEach(game => {
+      const { id, slug } = game.node
+      createPage({
+        path: slug,
+        component: path.resolve(`src/templates/game.js`),
+        context: {
+          id,
+          slug,
+        }
+      })
+    })
+    return result
+  })
+}
 exports.createResolvers = async ({
   actions,
   cache,
---
+++ src/templates/game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import { Wrapper, Image, Actorss, BottomEdgeUp } from '../pageStyles/pageStyles'
+import { COLORS } from "../constants"
+
+const GamesTemplate = () => {
+    return <div>
+        Niet afgekregen :)
+    </div>
+}
+
+export default GamesTemplate
